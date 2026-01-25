48a45781057ed37f526e556909dbbda57d57ec53
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-25 14:57:35 -0300

    Resolving correct folders, routes and structuring the form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import WellcomeMsg from "./components/WellcomeMsg"
 import { FaFileContract } from "react-icons/fa6";
 import '../src/styles/form/form_main.css'
 import './styles/global.css'
-
+import TextInfoContract from "./components/TextInfoContract";
 function App() {
 
 
@@ -13,12 +13,13 @@ function App() {
       <main>
         <form id="form_area">
           <header id="form_header">
-            <div className="text_header_form">
-              <h1>Seu site</h1>
-            </div>
             <div id="vy_logo"></div>
-            <FaFileContract size={36}/>
+            <FaFileContract id="icon_contract" size={36}/>
           </header>
+
+          <TextInfoContract />
+
+
         </form>
       </main>
      </>
---
+++ src/components/TextInfoContract.jsx
@@ -0,0 +1,18 @@
+import '../styles/form/text_info_contract.css'
+
+const TextInfoContract = () => {
+  return (
+    <>
+    <section id='text_info_contract_msg'>
+      <h2></h2>
+      <div>
+         <p></p>
+      </div>
+
+    </section>
+    </>
+  )
+}
+
+
+export default TextInfoContract
