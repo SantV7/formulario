9524594526ba0e1b0fc8969670cd60a5188a9012
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-25 15:21:13 -0300

    More styling

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
 
           <TextInfoContract />
 
-
         </form>
       </main>
      </>
--- src/components/TextInfoContract.jsx
+++ src/components/TextInfoContract.jsx
@@ -4,9 +4,10 @@ const TextInfoContract = () => {
   return (
     <>
     <section id='text_info_contract_msg'>
-      <h2></h2>
-      <div>
-         <p></p>
+      <h2>Faça e envie seu orçamento</h2>
+      <div id='paragraph_text_project'>
+         <p>Preencha com seus dados e o tipo de projeto que você tem em mente.</p>
+         <p>Caso só tenha a idéia, não tem problema, vamos discutir as idéias juntos</p>
       </div>
 
     </section>
