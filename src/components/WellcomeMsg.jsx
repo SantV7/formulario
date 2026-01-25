36675f70a2533e176453e0b317ad9c577edef10d
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-25 13:38:45 -0300

    Adding animation on icon intro

--- src/components/WellcomeMsg.jsx
+++ src/components/WellcomeMsg.jsx
@@ -1,5 +1,5 @@
 import '../styles/form/wellcome_msg.css'
-import { FaHeartCircleExclamation } from "react-icons/fa6";
+import { AiFillAliwangwang } from "react-icons/ai";
 
 const WellcomeMsg = () => {
 
@@ -9,10 +9,9 @@ const WellcomeMsg = () => {
     <header id='header'>
       <h2>Olá, eu sou <span className="highlight_border">Vinícius</span></h2>
       <p>Desenvolvedor <span className="highlight_border">Front-End</span> em busca da primeira oportunidade de estágio ou freelancer. <br />
-      Crio sites modernos, responsivos e com experiências incríveis pra você. <FaHeartCircleExclamation size={33} id='heart_intro'/>
+      Crio sites modernos, responsivos e com experiências incríveis pra você. <AiFillAliwangwang size={48} id='icon_intro'/>
       </p>
     </header>
-
     </>
   )
 }
