50477a72d79b3615c456c3fd8d9e0eb7238ea9ce
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-26 18:20:44 -0300

    ...

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,20 +7,15 @@ import { HiOutlineMail } from "react-icons/hi"; <HiOutlineMail /> // close email
 import { HiOutlineMailOpen } from "react-icons/hi"; <HiOutlineMailOpen /> // open email
 // Email icons
 
-import { PiEyeClosedDuotone } from "react-icons/pi"; <PiEyeClosedDuotone />  // close eye PI
-import { PiEyeDuotone } from "react-icons/pi"; <PiEyeDuotone /> // open eye
-// Password icons
 
 import '../styles/form/form.css'
 
 
-
-
 const Form = () => {
 
-  const [userIcon, setUserIcon] = useState(false)
-  const [emailIcon, setEmailIcon] = useState(false)
-  const [passwordIcon, setPasswordIcon] = useState(false)
+   const [userIcon, setUserIcon] = useState(false)
+   const [emailIcon, setEmailIcon] = useState(false)
+   const [passwordIcon, setPasswordIcon] = useState(false)
 
 
   return (
@@ -29,28 +24,35 @@ const Form = () => {
           <div className="area_input">
             <div className="input_data">
               <label htmlFor="name_user">Nome</label>
-              <input type="text" required name='name_user' onClick={() => setUserIcon(!userIcon)}  placeholder='Seu primeiro nome' id='name_input'/> 
-               {
-                userIcon
-                ? <FaUser />
-                : <FaRegUser />
-               }
+              <div className="input_display">
+                <input type="text" required name='name_user' onClick={() => setUserIcon(!userIcon)}
+                  placeholder='Seu primeiro nome' id='name_input'/>
+                 {
+                  userIcon
+                  ? <FaUser />
+                  : <FaRegUser />
+                 }
+              </div>
 
               <label htmlFor="email_user">Email</label>
-              <input type="email" required name="email_user" onClick={() => setEmailIcon(!emailIcon)} placeholder='Seu E-mail' id="email_input" /> 
-              {
-                emailIcon
-                ? <HiOutlineMailOpen />
-                : <HiOutlineMail />
-              }
+              <div className="input_display">
+                <input type="email" required name="email_user" onClick={() => setEmailIcon(!emailIcon)} 
+                placeholder='Seu E-mail' id="email_input" />
+                {
+                  emailIcon
+                  ? <HiOutlineMailOpen />
+                  : <HiOutlineMail />
+                }
+              </div>
             </div>
 
             <div className="input_data">
               <label htmlFor="cell_phone_input">Telefone</label>
-              <input type="tel" required name="cell_phone_input" id="cell_phone_input" />
+              <div className="input_display"><input type="tel" required name="cell_phone_input" id="cell_phone_input" /></div>
+              
 
               <label htmlFor="file_user"Anexar arquivo></label>
-              <input type="file" accept=".png,.jpg,.pdf,.doc" name="file_usesr" id="file_user_input" />l
+              <div className="input_display"><input type="file" accept=".png,.jpg,.pdf,.doc" name="file_usesr" id="file_user_input" /></div>
             </div>
           </div>
 
