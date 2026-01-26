20657847c90afc9215827badc00bb7ac72c3baf4
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-25 21:19:00 -0300

    more adjustments in style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import WellcomeMsg from "./components/WellcomeMsg"
 import { FaFileContract } from "react-icons/fa6";
-import '../src/styles/form/form_main.css'
+import '../src/styles/form/main_text.css'
 import './styles/global.css'
 import TextInfoContract from "./components/TextInfoContract";
 function App() {
@@ -11,15 +11,17 @@ function App() {
      <WellcomeMsg />
 
       <main>
-        <form id="form_area">
+        <div id="box_introdution_area">
           <header id="form_header">
             <div id="vy_logo"></div>
-            <FaFileContract id="icon_contract" size={36}/>
+            <FaFileContract id="icon_contract" size={39}/>
           </header>
-
+          
           <TextInfoContract />
+        </div>
 
-        </form>
+
+
       </main>
      </>
   )
--- src/components/TextInfoContract.jsx
+++ src/components/TextInfoContract.jsx
@@ -5,15 +5,16 @@ const TextInfoContract = () => {
     <>
     <section id='text_info_contract_msg'>
       <h2>Faça e envie seu orçamento</h2>
+
       <div id='paragraph_text_project'>
          <p>Preencha com seus dados e o tipo de projeto que você tem em mente.</p>
          <p>Caso só tenha a idéia, não tem problema, vamos discutir as idéias juntos</p>
+
          <div id='area_arrow'>
           <div></div>
-          <div className="arrow_right">
-
-          </div>
+          <div className="arrow_right"></div>
          </div>
+
       </div>
     </section>
     </>
