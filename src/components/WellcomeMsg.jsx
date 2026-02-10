d455d684c68416dda6b93f636f02796d04fd53c6
Vinícius Santos <3izuna@gmail.com>
2026-02-10 12:02:46 -0300

    Doying changes on background on header

--- src/components/WellcomeMsg.jsx
+++ src/components/WellcomeMsg.jsx
@@ -7,7 +7,6 @@ const WellcomeMsg = () => {
   return (
     <>
     <header id='header'>
-      <div className="header_background"></div>
       <h2>Opa, eu sou o <span className="highlight_border">Vinícius</span></h2>
       <p>Desenvolvedor <span className="highlight_border">Front-End</span> em busca da primeira oportunidade de estágio ou freelancer. <br />
       Crio sites modernos, responsivos e com experiências incríveis pra você. <AiFillAliwangwang size={48} id='icon_intro'/>
